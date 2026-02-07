04236c74b35bfb72b86827fa0f617d863180eca3
ashalathapasupuleti2007-crypto <ashalathapasupuleti2007@gmail.com>
2026-02-07 13:53:12 +0530

    Add files via upload

---
+++ Arthematic.java
@@ -0,0 +1,11 @@
+public class Arthematic {
+   public static void main(String[] args){
+    int a=25;
+    int b=35;
+    System.out.println("a+b");
+    System.out.println("a-b");
+    System.out.println("a*b");
+    System.out.println("a/b");
+    System.out.println("a%b");
+   } 
+}
