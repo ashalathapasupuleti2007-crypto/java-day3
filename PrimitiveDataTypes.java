765b7405ccc5d9b559b84241a376d11d4e4238bc
ashalathapasupuleti2007-crypto <ashalathapasupuleti2007@gmail.com>
2026-02-07 10:52:28 +0530

    Add files via upload

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,17 @@
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        char c='A';
+        float f=12.345f;
+        double d=345.87654d;
+        short a=111;
+        long e=1234;
+        boolean t=90<56;
+        System.out.println(c);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(a);
+        System.out.println(e);
+        System.out.println(t);
+
+    }
+}
